6278f90743ae9de6d84666052d0d72edd3552f85
Doli Bhanushali <doli@happychickoo.com>
2009-10-06 13:58:31 +0530

    Add video through flashembed.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

---
+++ media/script/index.js
@@ -0,0 +1,11 @@
+jQuery(function(){
+  flashembed('video_wrapper', {
+      src: 'http://www.youtube.com/v/_V2FV3Z7_EA&hl=en&fs=1&ap=%2526fmt%3D18&autoplay=1&rel=0&border=0&loop=0',
+      wmode: 'transparent',
+      loop: 'false',
+      allowFullScreen: 'true',
+      allowscriptaccess: 'always',
+      height: '350px',
+      width: '620px'
+  });
+});  
