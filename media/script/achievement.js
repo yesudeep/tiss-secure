60ae449cbf8929ad575389447cf1856a6a08f1be
Doli Bhanushali <doli@happychickoo.com>
2009-10-23 22:52:47 +0530

    Add the jquery innerfade file.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

---
+++ media/script/achievement.js
@@ -0,0 +1,9 @@
+jQuery(function(){
+    jQuery("#alumni_achievers").innerfade({
+      animationtype: 'fade',
+      speed: 'slow',
+      timeout: 3000,
+      type: 'sequence',
+      containerheight: '250px'
+    });
+});
