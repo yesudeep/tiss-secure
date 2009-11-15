544300fdf13c32b5bbf6f4e10c7c6b41874f20b6
Doli Bhanushali <doli@waterloo.(none)>
2009-11-15 19:01:45 +0530

    Imporve the styling of the pie chart and the statistic table.
    Signed-off-by: Doli Bhanushali <doli@waterloo.(none)>

--- media/script/reports.js
+++ media/script/reports.js
@@ -1,5 +1,11 @@
 jQuery(function(){
     //make some charts
-    jQuery('#summer_pie_chart').visualize({type: 'pie', pieMargin: 30, title: '2009 Total Sales by Individual'});	
-
+    jQuery('#summer_pie_chart, #final_pie_chart').visualize({
+        type: 'pie', 
+        pieMargin: 40, 
+        title: 'Sector Wise Split',
+        width: 800,
+        height:350,
+        colors: ['#5a0000','#740101', '#8f0b0b', '#a43200', '#ba4917', '#ba5c17', '#d9742b', '#e08727', '#ed891e', '#ed9d1e']
+    });	
 });
