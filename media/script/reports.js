d128389968eb35e22ad042fd9762be0acdb888ef
Doli Bhanushali <doli@waterloo.(none)>
2009-11-18 13:23:41 +0530

    Try to include the pie chart in tab navigation.
    Signed-off-by: Doli Bhanushali <doli@waterloo.(none)>

--- media/script/reports.js
+++ media/script/reports.js
@@ -1,11 +1,19 @@
 jQuery(function(){
     //make some charts
-    jQuery('#summer_pie_chart, #final_pie_chart').visualize({
+    jQuery('#summer_pie_chart').visualize({
         type: 'pie', 
         pieMargin: 40, 
         title: 'Sector Wise Split',
         width: 800,
         height:350,
         colors: ['#5a0000','#740101', '#8f0b0b', '#a43200', '#ba4917', '#ba5c17', '#d9742b', '#e08727', '#ed891e', '#ed9d1e']
-    });	
+    });
+    jQuery('#final_pie_chart').visualize({
+        type: 'pie', 
+        pieMargin: 40, 
+        title: 'Sector Wise Split',
+        width: 800,
+        height:350,
+        colors: ['#5a0000','#740101', '#8f0b0b', '#a43200', '#ba4917', '#ba5c17', '#d9742b', '#e08727', '#ed891e', '#ed9d1e']
+    });		
 });
