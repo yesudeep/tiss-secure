01a177b821affadf4388c2336d83a11ad5006e4f
Mrudul Deosthali <mrudul@walworth.(none)>
2009-11-26 16:57:10 +0530

    Add qtip on the address of the contact us in footer.
    Signed-off-by: Mrudul Deosthali <mrudul@walworth.(none)>

---
+++ media/script/contact_us.js
@@ -0,0 +1,22 @@
+jQuery(function() {
+  jQuery("#contact_us").qtip({
+    position: {
+      target: 'mouse',
+      corner: {
+        target: "bottomRight",
+        tooltip: "bottomMidlle"
+      }
+    },
+    style: {
+      width: 400,
+      padding: 5,
+      color: 'black',
+      textAlign: 'center',
+      border: {
+        radius: 5
+      },
+      name: 'light'
+    }
+  });
+});
+
